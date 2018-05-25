ecb176fa39ce801ca27d5d259f04bfbda932fd30
Himanshu Tyagi <tyagi.himanshu90@gmail.com>
2018-05-25 15:19:14 -0400

    Update linearRegression.py

--- Practice/linearRegression.py
+++ Practice/linearRegression.py
@@ -17,3 +17,36 @@ def estimate_coef(x, y):
     b_0 = m_y - b_1*m_x
 
     return (b_0, b_1)
+
+def plot_regression_line(x, y, b):
+    # plotting the actual points as scatter plot
+    plt.scatter(x, y, color= "m", marker = "o", s = 30)
+
+    # predict response vector
+    y_pred = b[0] + b[1]*x
+
+    # plotting the regression line
+    plt.plot(x, y_pred, color = "g")
+
+    # putting labels
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    # function to show plot
+    plt.show()
+
+def main():
+    # observation
+    x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
+ 
+    # estimating coefficients
+    b = estimate_coef(x, y)
+    print("Estimated coefficients:\nb_0 = {}  \
+          \nb_1 = {}".format(b[0], b[1]))
+ 
+    # plotting regression line
+    plot_regression_line(x, y, b)
+ 
+if __name__ == "__main__":
+    main()
